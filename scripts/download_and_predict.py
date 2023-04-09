0dfa11633ff313cd5a5028d0b7a8577724e47649
Ali Shirali <ali_9938@yahoo.com>
2023-04-09 05:43:54 -0700

    temp change for debug on server

--- scripts/download_and_predict.py
+++ scripts/download_and_predict.py
@@ -164,6 +164,8 @@ if __name__ == '__main__':
     subset_file_name = prefix + laionu.get_laion_subset_file_name(0, params['laion_until_part'])
 
     df = pd.read_parquet(os.path.join(params['laion_path'], subset_file_name))
+    # TODO
+    df = df.iloc[:10000]
 
     print_verbose('done!\n')
 
