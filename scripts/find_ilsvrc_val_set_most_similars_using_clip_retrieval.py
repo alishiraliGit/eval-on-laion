4a6893e990633f014486804e1c808afff41e759a
Ali Shirali <ali_9938@yahoo.com>
2023-04-15 14:24:39 -0700

    retrieving from clip retrieval can be continued

--- scripts/find_ilsvrc_val_set_most_similars_using_clip_retrieval.py
+++ scripts/find_ilsvrc_val_set_most_similars_using_clip_retrieval.py
@@ -9,6 +9,8 @@ from clip_retrieval.clip_client import ClipClient, Modality
 sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), '..'))
 
 import configs
+from utils import logging_utils as logu
+from utils.logging_utils import print_verbose
 
 
 if __name__ == '__main__':
@@ -27,10 +29,19 @@ if __name__ == '__main__':
     # Size
     parser.add_argument('--top_k', type=int, default=50)
 
+    # Logging
+    parser.add_argument('--no_verbose', dest='verbose', action='store_false')
+    parser.add_argument('--save_freq', type=int, default=1000)
+
+    # Continue
+    parser.add_argument('--no_continue', dest='continue', action='store_false')
+
     # Convert to dictionary
     params = vars(parser.parse_args())
 
     # ----- Init. -----
+    logu.verbose = params['verbose']
+
     os.makedirs(params['save_path'], exist_ok=True)
 
     # ----- Init. the client -----
@@ -46,15 +57,38 @@ if __name__ == '__main__':
         use_violence_detector=True
     )
 
+    # ----- Load previous results (if any) -----
+    results_file_name = f'top{params["top_k"]}_val_most_similars_from_{params["clip_retrieval_index_name"]}.json'
+
+    if os.path.exists(os.path.join(params['save_path'], results_file_name)) and params['continue']:
+        print_verbose('loading previous results to continue ...')
+
+        with open(os.path.join(params['save_path'], results_file_name), 'r') as f:
+            all_results = json.load(f)
+
+        # Convert str keys to int
+        all_results = {int(k): v for k, v in all_results.items()}
+
+        print_verbose(f'\tfound {len(all_results)} rows!')
+
+        print_verbose('done!\n')
+    else:
+        all_results = {}
+
     # ----- Retrieve -----
-    all_results = {}
     for idx in tqdm(range(1, configs.ILSVRCConfigs.NUM_VAL + 1)):
+        if idx in all_results:
+            continue
+
         results = client.query(image=os.path.join(params['images_path'], 'ILSVRC2012_val_%08d.JPEG' % idx))
 
         all_results[idx] = results
 
-    # ----- Save -----
-    with open(os.path.join(params['save_path'],
-                           f'top{params["top_k"]}_val_most_similars_from_{params["clip_retrieval_index_name"]}.json'
-                           ), 'w') as f:
-        json.dump(all_results, f)
+        # Save
+        if (idx % params['save_freq'] == 0) or (idx == configs.ILSVRCConfigs.NUM_VAL):
+            print_verbose('saving ...')
+
+            with open(os.path.join(params['save_path'], results_file_name), 'w') as f:
+                json.dump(all_results, f)
+
+            print_verbose('done!\n')
