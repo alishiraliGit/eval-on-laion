7bce51c56eb58c881ad5b864a674c7335b4d3bb0
Ali Shirali <ali_9938@yahoo.com>
2023-09-22 22:12:57 -0700

    added a wrapper for download_and_predict that reruns the script in the case of mp failure

---
+++ scripts/predict/download_and_predict_wrapper.py
@@ -0,0 +1,26 @@
+import subprocess
+import time
+import os
+import sys
+
+sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', '..'))
+
+
+while True:
+    try:
+        # Run script_1.py using subprocess with the same command-line arguments
+        result = subprocess.run(
+            ['python', 'scripts/predict/download_and_predict.py'] + sys.argv[1:],
+            text=True, check=True
+        )
+
+        # If download_and_predict ran successfully, break out of the loop
+        break
+    except subprocess.CalledProcessError as e:
+        # Handle exceptions raised by script_1.py
+        print(f'download_and_predict.py raised an exception: {e}')
+
+        time.sleep(3)
+
+print('download_and_predict.py ran successfully.')
+
