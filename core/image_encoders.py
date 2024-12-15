059762994a56bed4c73b5e41d41a7afa01b220f2
Ali Shirali <ali_9938@yahoo.com>
2024-12-15 08:58:39 -0800

    supporting new image encoders

--- core/image_encoders.py
+++ core/image_encoders.py
@@ -1,6 +1,7 @@
 import numpy as np
 import torch
-from transformers import ViTImageProcessor, ViTModel
+from transformers import ViTImageProcessor, ViTModel, BeitImageProcessor, BeitModel, \
+    ConvNextImageProcessor, ConvNextModel
 
 import configs
 from core.foundationmodels.clip import CLIP
@@ -30,6 +31,21 @@ def select_image_encoder(ver:  str):
         img_encoder = wrapper.image_embeds
         img_encoder_ver = ver
         img_encoder_batch_size = configs.CLIPConfig.BATCH_SIZE
+
+    elif ver.startswith('beit'):
+        wrapper = BEiTWrapper(ver)
+
+        img_encoder = wrapper.image_embeds
+        img_encoder_ver = ver
+        img_encoder_batch_size = configs.CLIPConfig.BATCH_SIZE
+
+    elif ver.startswith('convnext'):
+        wrapper = ConvNeXTWrapper(ver)
+
+        img_encoder = wrapper.image_embeds
+        img_encoder_ver = ver
+        img_encoder_batch_size = configs.CLIPConfig.BATCH_SIZE
+
     else:
         raise NotImplementedError
 
@@ -42,6 +58,48 @@ class ViTWrapper:
 
         self.processor = ViTImageProcessor.from_pretrained(f'google/{ver}')
         self.model = ViTModel.from_pretrained(f'google/{ver}')
+
+        self.model.to(ptu.device)
+
+    def image_embeds(self, images) -> np.ndarray:
+        inputs = self.processor(images=images, return_tensors='pt')
+
+        inputs = inputs.to(ptu.device)
+
+        with torch.no_grad():
+            outputs = self.model(**inputs)
+            embeds = outputs.pooler_output
+
+        return ptu.to_numpy(embeds)
+
+
+class BEiTWrapper:
+    def __init__(self, ver):
+        self.ver = ver
+
+        self.processor = BeitImageProcessor.from_pretrained(f'microsoft/{ver}')
+        self.model = BeitModel.from_pretrained(f'microsoft/{ver}')
+
+        self.model.to(ptu.device)
+
+    def image_embeds(self, images) -> np.ndarray:
+        inputs = self.processor(images=images, return_tensors='pt')
+
+        inputs = inputs.to(ptu.device)
+
+        with torch.no_grad():
+            outputs = self.model(**inputs)
+            embeds = outputs.pooler_output
+
+        return ptu.to_numpy(embeds)
+
+
+class ConvNeXTWrapper:
+    def __init__(self, ver):
+        self.ver = ver
+
+        self.processor = ConvNextImageProcessor.from_pretrained(f'facebook/{ver}')
+        self.model = ConvNextModel.from_pretrained(f'facebook/{ver}')
 
         self.model.to(ptu.device)
 
